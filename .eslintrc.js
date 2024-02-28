d629e93408a0bfe1d9bc332961a3f993d2ba8384
vinu <vinusysops@gmail.com>
2024-02-28 15:20:44 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: 'eslint:recommended',
+    rules: {
+        'no-console': 'off',
+        // Add more rules as needed
+    },
+};
+
